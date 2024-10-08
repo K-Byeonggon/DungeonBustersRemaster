f26e02c875f6dda99b2e6d8eae032faf0c2ef544
강병곤 <govleen79@gmail.com>
2024-10-08 18:10:20 +0900

    [fix] 공격 실패 플레이어가 있을 때 GetReward에서 null에러 수정

--- DungeonBustersRemaster/Assets/Scripts/UI/GameScene/UI_GetReward.cs
+++ DungeonBustersRemaster/Assets/Scripts/UI/GameScene/UI_GetReward.cs
@@ -44,6 +44,7 @@ public class UI_GetReward : MonoBehaviour
         //GameLogicManger에 신호보내기
         //GameLogicManager.Instance.CmdRegisterGetRewardChecked(NetworkClient.localPlayer.netId);
         GameLogicManager.Instance.CmdCheckConfirm(NetworkClient.localPlayer.netId, ConfirmPhase.GetRewardResult);
+        UI_WaitForOther.Show("다른 플레이어가 보상 받는 것을 기다리고 있습니다.");
     }
 
     private void SetLayoutReward(List<int> reward, int currentRank)
@@ -74,29 +75,34 @@ public class UI_GetReward : MonoBehaviour
     {        
         MyPlayerGameData playerGameData = NetworkClient.localPlayer.GetComponent<MyPlayerGameData>();
         int currentRank = playerGameData.CurrentStageRank;
-
         Debug.Log($"<color=red> currentRank: {currentRank} </color>");
 
-        //공격에 실패하거나, 순위가 최대 보상개수(3개) 보다 큰 플레이어들
         List<List<int>> rewards = MonsterDataManager.Instance.LoadedMonsters[currentMonsterDataId].Reward;
 
         Debug.Log($"<color=red> rewards.Count: {rewards.Count} </color>");
 
-        //순위가 실제 보상개수(1~3개) 이하인 플레이어
-        List<int> reward = rewards[currentRank];
-        if (currentRank >= 0 && reward != null && reward.Count > 0)
-        {
-            UIManager.Instance.ShowUI(UIPrefab.GetRewardUI);
-            UI_GetReward getRewardUI = UIManager.Instance.GetActiveUI(UIPrefab.GetRewardUI).GetComponent<UI_GetReward>();
 
-            getRewardUI.SetLayoutReward(reward, currentRank);
-        }
-        else
+        //공격에 성공한 플레이어와 보상을 받을 가능성이 있는(3등이내) 플레이어
+        if (currentRank >= 0 && currentRank < 3)
         {
-            UI_WaitForOther.Show("다른 플레이어가 보상 받는 것을 기다리고 있습니다.");
-            //LogicManager에 확인 신호보내기
-            GameLogicManager.Instance.CmdCheckConfirm(NetworkClient.localPlayer.netId, ConfirmPhase.GetRewardResult);
+            List<int> reward = rewards[currentRank];
+            //자신 순위의 보상이 있는(reward.Count > 0) 플레이어
+            if (reward != null && reward.Count > 0)
+            {
+                UIManager.Instance.ShowUI(UIPrefab.GetRewardUI);
+                UI_GetReward getRewardUI = UIManager.Instance.GetActiveUI(UIPrefab.GetRewardUI).GetComponent<UI_GetReward>();
+
+                getRewardUI.SetLayoutReward(reward, currentRank);
+
+                return;
+            }
         }
+
+        //보상을 못받는 경우
+        UI_WaitForOther.Show("다른 플레이어가 보상 받는 것을 기다리고 있습니다.");
+        //LogicManager에 확인 신호보내기
+        GameLogicManager.Instance.CmdCheckConfirm(NetworkClient.localPlayer.netId, ConfirmPhase.GetRewardResult);
+        
     }
 
 }
